c3b115b708ccffe3b13d5ae074aced5dd4d5714a
sefiKraus <sefy1600@gmail.com>
2016-09-08 16:18:28 +0300

    changes to solution- Serializable

--- my algorithms/src/algorithms/search/Dfs.java
+++ my algorithms/src/algorithms/search/Dfs.java
@@ -15,7 +15,6 @@ public class Dfs<T> extends CommonStackSearcher<T> {
 	@Override
 	public Solution<T> search(Searchable<T> searchable) {
 		
-		State<T>tempPos;
 		State<T>curPosition;
 		curPosition=searchable.getStartState();
 		ArrayList<State<T>>neighbors=new ArrayList<State<T>>();	
--- my algorithms/src/algorithms/search/Solution.java
+++ my algorithms/src/algorithms/search/Solution.java
@@ -1,6 +1,7 @@
 
 package algorithms.search;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 /**
  * 
@@ -8,11 +9,42 @@ import java.util.ArrayList;
  * @author Krausz Sefi 305371320
  * @since 02/08/2016
  */
-public class Solution<T> {
+@SuppressWarnings("serial")
+public class Solution<T> implements Serializable {
 
 	private ArrayList<T> solution;
 
+	public Solution() {
+	}
 	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((solution == null) ? 0 : solution.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Solution other = (Solution) obj;
+		if (solution == null) {
+			if (other.solution != null)
+				return false;
+		} else if (!solution.equals(other.solution))
+			return false;
+		return true;
+	}
+
+
 	public Solution(ArrayList<T> solution) {
 		super();
 		this.solution = solution;
