f42cb8e751405a13a6f5b670a40924e91eb55fee
sefiKraus <sefy1600@gmail.com>
2016-09-14 20:11:22 +0300

    init client side

---
+++ clientProject/src/presenter/Presenter.java
@@ -0,0 +1,14 @@
+package presenter;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Presenter implements Observer{
+
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ clientProject/src/view/CommonClassView.java
@@ -0,0 +1,5 @@
+package view;
+
+abstract class CommonClassView implements ClientView{
+
+}
